4ff40213cae3d2ff689ec89fedbf080a896c34cb
PrzemekPolak <przemekpolak138@gmail.com>
2020-03-18 14:48:44 +0100

    PP 2020-03-18

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 #adres URL przykładowej strony z opiniami
 url = "https://www.ceneo.pl/7468416#tab=reviews"
-                          #/50562230
+                          
 #pobranie kodu html strony z  zapodanego URl
 page_response = requests.get(url)
 page_tree = BeautifulSoup(page_response.text, 'html.parser')
@@ -12,20 +12,33 @@ page_tree = BeautifulSoup(page_response.text, 'html.parser')
 #wydobycie z kodu html fragmentów odpowiadających odpowiednim opiniom
 opinions = page_tree.find_all("li", "review-box")
 
-#wydobycie składowych dlapojedyńczych opinii
-opinion = opinions.pop()
-
-opinion_id = opinion["data-entry-id"]
-author = opinion.find("div",  "reviewer-name-line").string
-recommendation = opinion.find("div", "product-review-summary").find("em").string
-stars = opinion.find('span', 'review-score-count').string
-purchased = opinion.find('div','product-review-pz').string
-# - data wystawienia: span.review-time > time["datetime"] - pierwszy element listy
-# - data zakupu: span.review-time > time["datetime"] - drugi element listy
-useful = opinion.find('button', 'vote-yes').find('span').string
-useless = opinion.find('button', 'vote-no').find('span').string
-content = opinion.find('p', 'product-review-body').get_text()
-
-
-# - wady: div.cons-cell > ul
-# - zalety: div.pros-cell > ul
+#wydobycie składowych dla pojedyńczych opinii
+for opinion in opinions:
+    opinion_id = opinion["data-entry-id"]
+    author = opinion.find("div",  "reviewer-name-line").string
+    try:
+        recommendation = opinion.find("div", "product-review-summary").find("em").string
+    except AttributeError:
+        recommendation = None
+    stars = opinion.find('span', 'review-score-count').string
+    try:
+        purchased = opinion.find('div','product-review-pz').string
+    except AttributeError:
+        purchased = None
+    dates = opinion.find('span', 'review-time').find_all('time')
+    review_date = dates.pop(0)["datetime"]
+    try:
+        purchase_date = dates.pop(0)["datetime"]
+    except IndexError:
+        purchase_date = None
+    useful = opinion.find('button', 'vote-yes').find('span').string
+    useless = opinion.find('button', 'vote-no').find('span').string
+    content = opinion.find('p', 'product-review-body').get_text()
+    try:
+        pros = opinion.find('div', 'pros-cell').find('ul').get_text() 
+    except AttributeError:
+        pros = None
+    try:
+        cons = opinion.find('div', 'cons-cell').find('ul').get_text()
+    except AttributeError:
+        cons = None
